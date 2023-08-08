d777aad2ce4e2e2c96659761e2e30536cde11f83
santiago <santiagobenitez467@gmail.com>
2023-08-08 16:56:24 -0500

    feat: modified: src/index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
+const cors = require('cors');
 const express = require('express');
+const path = require('path');
 const app = express();
 
-//settings
-app.set('port', process.env.PORT || 3000);
-app.engine('html',)
+const indexRoutes = require('./routes/index')
+const tasksRoutes = require('./routes/tasks')
 
+//settings
+app.set('views', path.join(__dirname, 'views'));
+app.set('port', process.env.PORT || 3000);
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+//routes
+app.use(indexRoutes);
+app.use('/api',tasksRoutes);
 
 app.listen(app.get('port'), () => {
     console.log('server on port', app.get('port'));
